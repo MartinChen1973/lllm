46b69d60499af3ca4f6ef45f0f9cc3e61904683e
Martin Chen <cheny@cheny.com>
2025-04-18 22:01:05 +0800

    Added ollama_deepseek_env.py

---
+++ src/lesson08_ollama/ollama_deepseek_env.py
@@ -0,0 +1,16 @@
+import requests
+from langchain_ollama import OllamaLLM
+
+##### 访问本地Ollama API并查询可用模型
+BASE_URL = "http://localhost:11434/v1"  # 本地Ollama API URL
+models = requests.get(f"{BASE_URL}/models", timeout=10).json()["data"]
+
+for model_data in models:
+    print(f"Model ID: {model_data['id']}")
+
+##### 调用模型 
+model = OllamaLLM(model="deepseek-r1:1.5b")
+
+response = model.invoke("请给我讲一个关于猫的笑话。")
+
+print(response)
